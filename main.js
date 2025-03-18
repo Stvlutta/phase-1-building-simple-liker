d3359e0f1390571ce7dfda852146586a1430d7cc
Stvlutta <stvlutta@gmail.com>
2025-03-18 09:59:58 +0300

    complete lab

--- main.js
+++ main.js
@@ -4,14 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
 
-
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.innerText === EMPTY_HEART) {
+            heart.innerText = FULL_HEART;
+            heart.classList.add('activated-heart');
+          } else {
+            heart.innerText = EMPTY_HEART;
+            heart.classList.remove('activated-heart');
+          }
+        })
+        .catch((error) => {
+          errorModal.classList.remove('hidden');
+          errorModal.querySelector('#modal-message').innerText = error;
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
